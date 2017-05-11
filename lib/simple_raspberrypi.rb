2490c2395c1c22d397fe7a187cd6ef6f4b6c100e
Your Name <you@example.com>
2017-05-11 19:17:21 +0100

    Removed the Void class since it is now part of the PinX gem.

--- lib/simple_raspberrypi.rb
+++ lib/simple_raspberrypi.rb
@@ -11,13 +11,6 @@ require 'rpi_pinout'
 
 class SimpleRaspberryPi  
     
-  class Void
-    def on(duration=nil)               end
-    def off()                          end
-    def blink(seconds=0, duration=nil) end
-    alias stop off
-  end  
-
   def initialize(x=[])
     
     a = case x
@@ -37,7 +30,7 @@ class SimpleRaspberryPi
 
       if i.to_i >= self.length then
         puts "RPi warning: RPiPinOut instance #{i.inspect} not found"
-        Void.new
+        PinX::Void.new
       else
         self.at(i)
       end 
@@ -48,7 +41,6 @@ class SimpleRaspberryPi
   def pin()   @pins.first  end
   def pins()  @pins        end
     
-
 end
 
 
--- simple_raspberrypi.gemspec
+++ simple_raspberrypi.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'simple_raspberrypi'
-  s.version = '0.4.1'
+  s.version = '0.4.2'
   s.summary = 'Has the same features as the rpi gem, but relies upon the rpi_pinout gem instead of the pi_piper gem.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/simple_raspberrypi.rb']  
@@ -8,6 +8,6 @@ Gem::Specification.new do |s|
   s.signing_key = '../privatekeys/simple_raspberrypi.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/simple_raspberrypi'
 end
